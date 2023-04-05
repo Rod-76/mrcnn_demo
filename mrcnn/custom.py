a7d46be17828581e34254fa9cbb5a8d63f009c35
Rod-76 <82039720+Rod-76@users.noreply.github.com>
2023-04-05 16:04:43 +0800

    a

--- mrcnn/custom.py
+++ mrcnn/custom.py
@@ -1,7 +1,12 @@
 import os
 import sys
 import random
+import math
+import re
+import time
 import numpy as np
+import cv2
+import matplotlib
 import matplotlib.pyplot as plt
 import json
 import os
@@ -10,14 +15,14 @@ import zipfile
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("/content/mrcnn_demo")
-print("Custom MaskRCNN Loader")
+print("Mask RCNN Loader V1")
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
-from config import Config
-import utils
-import model as modellib
-import visualize
-from model import log
+from mrcnn.config import Config
+import mrcnn.utils
+import mrcnn.model as modellib
+import mrcnn.visualize
+from mrcnn.model import log
 from PIL import Image, ImageDraw
 
 import warnings
@@ -28,7 +33,7 @@ warnings.filterwarnings('ignore')
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_oyster.h5")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
@@ -57,7 +62,7 @@ class CustomConfig(Config):
     IMAGES_PER_GPU = 4
 
     # Number of classes
-    NUM_CLASSES = 1 
+    NUM_CLASSES = 1
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
